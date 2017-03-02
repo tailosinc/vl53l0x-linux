5891aeef0f5641ad5679aae367d3c5c0041a2149
mjbogusz <mjbogusz+github@gmail.com>
2017-03-02 16:32:12 +0100

    examples: added singleMinimal example

---
+++ examples/singleMinimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <VL53L0X.h>
+
+int main() {
+	// Create and initialize the sensor
+	VL53L0X sensor;
+	sensor.init();
+	sensor.setTimeout(200);
+
+	for (int i = 0; i < 1000; ++i) {
+		uint16_t distance = sensor.readRangeSingleMillimeters();
+		if (sensor.timeoutOccurred()) {
+			std::cout << "timeout!" << std::endl;
+		} else {
+			std::cout << distance << std::endl;
+		}
+	}
+
+	return 0;
+}
